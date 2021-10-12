b9330f2da44cd07cec89144ea268ba2c5fca1310
Aswin Rajeev <aswin.rajeev@liferay.com>
2021-10-12 10:10:32 +0100

    Updates

---
+++ precache-manifest.6b4c464d39e6ac6d883b708ab7bbef54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5930815aa713c17d8e3",
+    "url": "/static/css/main.5d6d13c9.chunk.css"
+  },
+  {
+    "revision": "e5930815aa713c17d8e3",
+    "url": "/static/js/main.e5930815.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92d453a39b2fedbb91457d5564e58281",
+    "url": "/index.html"
+  }
+];
